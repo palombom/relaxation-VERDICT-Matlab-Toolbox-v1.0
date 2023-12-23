264589028a961899d9b2fc433237fd69de0e8754
Marco Palombo <33546086+palombom@users.noreply.github.com>
2023-12-23 18:41:46 +0000

    Add files via upload

---
+++ main_rVERDICT_analysis.m
@@ -0,0 +1,36 @@
+function data = main_rVERDICT_analysis(DataFolder, pythonpath)
+
+disp(['//////   rVERDICT analysis of data: ' DataFolder '   \\\\\\'])
+
+addpath(genpath('src'));
+rng(123) % For reproducibility
+
+%% Load the data
+disp('   - Loading Data')
+DataFilename = 'data';
+data = load_data(DataFolder, DataFilename);
+disp('   [DONE]')
+%% Prepare data for DL fitting
+disp('   - Preparing the Data for DL Fitting')
+data = make_direction_average(data);
+disp('   [DONE]')
+%% Train DL model
+disp('   - Training MLP')
+SNR = 35;
+train_MLP(data, SNR, pythonpath);
+disp('   [DONE]')
+%% Fit DL model
+disp('   - Fitting MLP')
+data = fit_MLP(data, pythonpath);
+disp('   [DONE]')
+%% Save maps
+disp('   - Saving rVERDICT maps as nifti')
+OutputFolder = fullfile(DataFolder, 'rVERDICT_output');
+save_rVERDICT_maps(data, OutputFolder)
+disp('   [DONE]')
+disp('//////   FINISHED   \\\\\\')
+%% Show example
+tmp = load_untouch_nii(fullfile(OutputFolder, 'f_ic.nii.gz'));
+figure, imshow(tmp.img(:,:,8), [0 1])
+
+end
